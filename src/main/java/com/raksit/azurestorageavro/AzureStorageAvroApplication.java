7a316e732c9611e996fbfca78a813df4dacb914a
Raksit <raksit.m@ku.th>
2020-04-09 14:33:16 +0700

    :tada:

---
+++ src/main/java/com/raksit/azurestorageavro/AzureStorageAvroApplication.java
@@ -0,0 +1,13 @@
+package com.raksit.azurestorageavro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureStorageAvroApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AzureStorageAvroApplication.class, args);
+  }
+
+}
